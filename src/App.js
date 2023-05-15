9c3253b7967d48b64edf557d10e359d7854a0fb7
ChrisMoura2000 <chris.tryber@gmail.com>
2023-05-15 17:44:39 -0300

    create component stocksandbusiness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 import { Header } from './components/Header'
+import StocksAndBusiness from './components/StocksAndBusiness';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <StocksAndBusiness/>
     </div>
   );
 }
---
+++ src/components/StocksAndBusiness.jsx
@@ -0,0 +1,13 @@
+function StocksAndBusiness() {
+    const style = {
+        div: `ml-5`,
+        title: `text-dark`
+    }
+    return (
+        <div className={style.div}>
+            <h1 className={style.title}>ações e negócios</h1>
+        </div>
+    )
+}
+
+export default StocksAndBusiness
